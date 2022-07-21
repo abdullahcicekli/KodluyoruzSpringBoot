d138f7ecf3cc45086a04b6bcf011a990b834ac9a
abdullah.cicekli <abdulah.cicekli@sahibinden.com>
2022-07-21 19:48:44 +0300

    for dev tools config

---
+++ src/main/java/com/abdullahcicekli/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.abdullahcicekli.utils;
+
+public class ConfigProgramming {
+    // DevTools ==> LiveReload (Dev tools çalışabilmesi için gerekli ayarlar.)
+    // file -> Preferences -> compiler -> Build project auto
+    // file -> Preferences -> Advanced Settings -> Allow make to start even
+}
