1531753e378cf608eac9dd2d2b5e6555c6a86969
abdullah.cicekli <abdulah.cicekli@sahibinden.com>
2022-07-22 15:56:08 +0300

    Thymeleaf ile model model list model object gönderme

--- src/main/java/com/abdullahcicekli/controller/ThymeleafController.java
+++ src/main/java/com/abdullahcicekli/controller/ThymeleafController.java
@@ -1,37 +1,71 @@
 package com.abdullahcicekli.controller;
 
+import com.abdullahcicekli.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/thymeleaf1")
     //@ResponseBody
-    public String getThymeleaf1(){
-        return "thymeleaf1" ;
+    public String getThymeleaf1() {
+        return "thymeleaf1";
     }
+
     @GetMapping("thymeleaf2")
-    public String getModel(Model model){
-        model.addAttribute("isim","Sueda");
-        model.addAttribute("isim","Aminenur");
+    public String getModel(Model model) {
+        model.addAttribute("isim", "Sueda");
+        model.addAttribute("isim", "Aminenur");
 
         return "thymeleaf1";
     }
+
+    // birden fazla model göndermek
     @GetMapping("thymeleaf3")
-    public String getModel2(Model model){
-        model.addAttribute("isim","Sueda");
-        model.addAttribute("isim","Aminenur");
+    public String getModel2(Model model) {
+        model.addAttribute("isim1", "Sueda");
+        model.addAttribute("isim", "Aminenur");
 
         return "/thymeleaf_file/thymeleaf2";
     }
 
     @GetMapping("thymeleaf4")
-    public String getModel4(Model model){
-        model.addAttribute("isim","Sueda");
+    public String getModel4(Model model) {
+        model.addAttribute("isim", "Sueda");
 
         return "thymeleaf4";
     }
+
+    /// modelde object gönderme
+    @GetMapping("/thymeleaf5")
+    public String thymeleafObject(Model model){
+        model.addAttribute("key","value");
+        ProductDto productDto = ProductDto.builder()
+                .productName("Iphone")
+                .productPrice(23599.9)
+                .id(0L).build();
+
+        model.addAttribute("key2",productDto);
+        return "thymeleaf5";
+    }
+
+    /// modelde object list gönderme
+    @GetMapping("/thymeleaf6")
+    public String thymeleafObjectList(Model model){
+        List<ProductDto> listem = new ArrayList<>();
+        listem.add(ProductDto.builder().productName("Iphone").productPrice(23599.9).id(0L).build());
+        listem.add(ProductDto.builder().productName("Samsung").productPrice(16999.9).id(1L).build());
+        listem.add(ProductDto.builder().productName("Xiomi").productPrice(14999.9).id(2L).build());
+
+        model.addAttribute("productListe",listem);
+        return "thymeleaf6";
+    }
+
+
 }
---
+++ src/main/java/com/abdullahcicekli/dto/ProductDto.java
@@ -0,0 +1,19 @@
+package com.abdullahcicekli.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class ProductDto {
+    private Long id;
+    private String productName;
+    private Double productPrice;
+
+}
